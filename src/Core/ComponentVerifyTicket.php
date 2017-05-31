c9e6e0305978f6a9b7e81d35923c8756fdb60725
JH-W <wangj4543@163.com>
2017-05-31 16:44:11 +0800

    test get ticket

--- src/Core/ComponentVerifyTicket.php
+++ src/Core/ComponentVerifyTicket.php
@@ -3,12 +3,21 @@
  * @Author: binghe
  * @Date:   2017-05-31 14:24:24
  * @Last Modified by:   binghe
- * @Last Modified time: 2017-05-31 14:40:53
+ * @Last Modified time: 2017-05-31 16:43:31
  */
 namespace Binghe\Wechat\Core;
 use Binghe\Wechat\Traits\CacheTrait;
 class ComponentVerifyTicket
 {
     use CacheTrait;
-    
+    /**
+     *
+     * get verify ticket
+     *
+     * @return string
+     */
+    public function getVerifyTicket()
+    {
+        return 'test ticket';
+    }
 }
